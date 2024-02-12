542baf5b2b523104ba60d75f349792df40ef144e
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2024-02-12 20:46:44 +0530

    Small changes in next-auth branch

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,8 +3,17 @@ import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions : NextAuthOptions= {
   // Configure one or more authentication providers
+  secret: "sarthak_is_turbo",
   session: {
     strategy: "jwt",
+  },
+  callbacks: {
+    async signIn(params) {
+      if (params.account) {
+        const cookie = params.account.id_token;
+        return true;
+      } return false;
+    },
   },
   providers: [
     GoogleProvider({
